afec21408c4b7bafe97d25d857e653675c5ae85e
pneedleman <paul.needleman@gmail.com>
2021-02-16 23:05:00 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -9,7 +9,7 @@
  * Exit with an error message if the CONSUMER_KEY or CONSUMER_SECRET is not defined.
  */
 require_once('config.php');
-if (CONSUMER_KEY === 'nyjm89DDyYBZVtZSk1mCw' || CONSUMER_SECRET === 'rJ8O5SPdcQDQ7nSNfFXgeyU9vbt8oEMhMnPo9enecvw') {
+if (CONSUMER_KEY === '' || CONSUMER_SECRET === '') {
   echo 'You need a consumer key and secret to test the sample code. Get one from <a href="https://twitter.com/apps">https://twitter.com/apps</a>';
   exit;
 }
